f59f3872bf680b0e799c8cb5f1efede2a73f9d5a
Oriana <oriana.nohemi22@gmail.com>
2020-10-13 21:32:02 -0500

    header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  brand = 'assets/image/brand.svg';
+
   constructor() { }
 
   ngOnInit(): void {
