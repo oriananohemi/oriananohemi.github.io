ae360e3f046f9eca02bf54342167bc69c4a5ce85
Oriana <oriana.nohemi22@gmail.com>
2020-10-17 13:37:10 -0500

    estilos generales

--- src/app/projects/projects.constant.ts
+++ src/app/projects/projects.constant.ts
@@ -1,34 +1,50 @@
 export const projects = [
   {
     title: 'Burger Queen',
-    image: 'assets/image/burgerQueen.png'
+    image: 'assets/image/burgerQueen.png',
+    demo: '',
+    github: 'https://github.com/oriananohemi/BOG001-burger-queen',
   },
   {
     title: 'SolSalud',
-    image: 'assets/image/solSalud.png'
+    image: 'assets/image/solSalud.png',
+    demo: 'https://oriananohemi.github.io/SolSalud/#/',
+    github: 'https://github.com/oriananohemi/SolSalud',
   },
   {
     title: 'PetShop',
-    image: 'assets/image/petShop.png'
+    image: 'assets/image/petShop.png',
+    demo: '',
+    github: 'https://github.com/oriananohemi/petShop',
   },
   {
     title: 'PhySport',
-    image: 'assets/image/phySport.png'
+    image: 'assets/image/phySport.png',
+    demo: 'https://oriananohemi.github.io/BOG001-social-network/',
+    github: 'https://github.com/oriananohemi/BOG001-social-network',
   },
   {
     title: 'Pokedex',
-    image: 'assets/image/pokedex.png'
+    image: 'assets/image/pokedex.png',
+    demo: 'https://oriananohemi.github.io/BOG001-data-lovers/',
+    github: 'https://github.com/oriananohemi/BOG001-data-lovers',
   },
   {
     title: 'Twyno',
-    image: 'assets/image/Twyno.png'
+    image: 'assets/image/Twyno.png',
+    demo: '',
+    github: '',
   },
   {
     title: 'Tu Closet',
-    image: 'assets/image/tuCloset.png'
+    image: 'assets/image/tuCloset.png',
+    demo: 'https://oriananohemi.github.io/tuCloset/',
+    github: 'https://github.com/oriananohemi/tuCloset',
   },
   {
     title: 'Pinterest',
-    image: 'assets/image/pinterest.png'
+    image: 'assets/image/pinterest.png',
+    demo: '',
+    github: '',
   },
 ]
